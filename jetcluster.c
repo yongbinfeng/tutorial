51527350feb7ca75b564e54b7d252fdd79df31d9
Yongbin Feng <yongbinfeng@Yongbins-MacBook-Pro.local>
2016-09-24 22:12:51 -0400

    corrected version

--- jetcluster.c
+++ jetcluster.c
@@ -1,17 +1,20 @@
 #include "TFile.h"
 #include "vector"
 
-/*int antikT(vector<float> *pt, vector<float> *eta, vector<float>*phi, vector<float> *mass){
+int antikT(vector<float> *pt, vector<float> *eta, vector<float>*phi, vector<float> *mass){
     return 1;
 }
-*/
 
 void jetcluster(){
   
-  TFile *f= new TFile("/home/cmsumd/Desktop/exercise/Jetcluster/for_jet_clustering_exercise.root");
+  TFile *f= new TFile("for_jet_clustering_exercise.root");
   TTree *t1 = (TTree*)f->Get("tree");
   
-  vector<float> *pt, *eta, *phi, *mass;
+  vector<float> *pt = 0;// pointer muast be initialized before passed as branch address
+  vector<float> *eta = 0;
+  vector<float> *phi = 0;
+  vector<float> *mass = 0;
+
   t1->SetBranchAddress("pt",   &pt);
   t1->SetBranchAddress("eta",  &eta);
   t1->SetBranchAddress("phi",  &phi);
@@ -19,9 +22,8 @@ void jetcluster(){
 
   Int_t nentries = (Int_t)t1->GetEntries();
   cout<<nentries<<endl;
-  for(Long64_t i=0; i<nentries; i++){
+  for(Int_t i=0; i<nentries; i++){
      t1->GetEntry(i);   // i.th event
-     cout<<(*pt)[0]<<endl;
       
      
   
