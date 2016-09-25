dab7084d729f5e1a7ff76850234b4a62de04c23a
yongbinfeng <ybfeng94@gmail.com>
2016-09-24 21:57:11 -0400

    error

--- jetcluster.c
+++ jetcluster.c
@@ -1,22 +1,27 @@
 #include "TFile.h"
 #include "vector"
 
+/*int antikT(vector<float> *pt, vector<float> *eta, vector<float>*phi, vector<float> *mass){
+    return 1;
+}
+*/
 
 void jetcluster(){
   
-  TFile *f= new TFile("for_jet_clustering_exercise.root");
-  TTree *tree = (TTree*)f->Get("tree");
+  TFile *f= new TFile("/home/cmsumd/Desktop/exercise/Jetcluster/for_jet_clustering_exercise.root");
+  TTree *t1 = (TTree*)f->Get("tree");
   
   vector<float> *pt, *eta, *phi, *mass;
-  tree->SetBranchAddress("pt",   &pt);
-  tree->SetBranchAddress("eta",  &eta);
-  tree->SetBranchAddress("phi",  &phi);
-  tree->SetBranchAddress("mass", &mass);
+  t1->SetBranchAddress("pt",   &pt);
+  t1->SetBranchAddress("eta",  &eta);
+  t1->SetBranchAddress("phi",  &phi);
+  t1->SetBranchAddress("mass", &mass);
 
-  Int_t nentries = (Int_t)tree->GetEntries();
-  for(Int_t i=0; i<nentries; i++){
-     tree->GetEntry(i);   // i.th event
-
+  Int_t nentries = (Int_t)t1->GetEntries();
+  cout<<nentries<<endl;
+  for(Long64_t i=0; i<nentries; i++){
+     t1->GetEntry(i);   // i.th event
+     cout<<(*pt)[0]<<endl;
       
      
   
